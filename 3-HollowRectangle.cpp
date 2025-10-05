46c5e78d92b4d60f6dc84e6b25abd73226da78cc
Sirisha Bantupakiri <bsirisha241@gmail.com>
2025-10-05 16:09:58 +0530

    Create 3-HollowRectangle.cpp

---
+++ 3-HollowRectangle.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int rows, cols; 
+     cout<<"enter rows " <<endl;
+     cin>>rows;
+     cout<<"enter cols"<<endl;
+     cin>>cols;
+     for(int i =0;i<rows; i++){
+        for(int j =0; j<cols; j++){
+            if(i ==0 || i==rows-1 ){
+                cout<< "*";
+            }
+           else if(j ==  0|| j== cols-1){
+            cout<<"*";
+           }
+           else{
+            cout<<" ";
+           }
+        }
+        cout<<endl;
+     }
+
+}
