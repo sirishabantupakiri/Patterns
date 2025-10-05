83e39e481f75bc5797b4542aae8f5fe0e34010d4
Sirisha Bantupakiri <bsirisha241@gmail.com>
2025-10-05 16:07:54 +0530

    Create Rectangle.cpp

---
+++ Rectangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int row =0;row<n;row++){
+        for(int col =0; col<m; col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
