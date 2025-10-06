392e374fb4b25e11c9e40efa2fb738d37b1a0e4b
Sirisha Bantupakiri <bsirisha241@gmail.com>
2025-10-06 12:47:45 +0530

    Create 11-HollowPyramid.cpp

---
+++ 11-HollowPyramid.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n - row - 1; col++) {
+            cout << " ";
+        }
+        for (int col = 0; col < 2 * row + 1; col++) {
+            if (col == 0 || col == 2 * row) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
